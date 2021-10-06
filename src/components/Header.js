87901dadc7045364b1ee0b61e8ba956e84b56ecd
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-06 15:09:08 -0400

    created header and added react and function header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="jumbotron">
+      <h1>Nicholas Chin</h1>
+    </header>
+  );
+}
+
+export default Header;
