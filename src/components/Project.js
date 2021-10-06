9b1f40786dc067ece035170c46f6434a51604ee7
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-06 17:08:15 -0400

    added project titles and descriptions

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,16 +9,16 @@ function Project() {
         {/* Card 1 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Baseball Direct</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
-              alt="boohoo"
+              alt="screenshot of baseball direct"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Group project using multiple APIs and Bulma</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
@@ -30,16 +30,16 @@ function Project() {
         {/* Card 2 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Stock Shout</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
               alt="boohoo"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Created using with Node, Express, Sequelize, MySQL, and Handlebars. Stock Shout allows the user to create an account, login, save a list of apps to their watchlist, and view tweets and stock information</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
@@ -51,16 +51,16 @@ function Project() {
         {/* Card 3 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Work Day Scheduler</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
               alt="boohoo"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Created using third party APIs like Moment.js, Twitter-Bootstrap, and Google Fonts</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
@@ -72,16 +72,16 @@ function Project() {
         {/* Card 4 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Weather Dashboard</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
               alt="boohoo"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Created using server-side API from OpenWeather API to display the current weather and 5 day forecasts for selected cities</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
@@ -93,16 +93,16 @@ function Project() {
         {/* Card 5 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Note Taker</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
               alt="boohoo"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Created using HTML, CSS, Bootstrap, Node and Express. Note Taker is hosted on Heroku and the app allows the user to create and notes.</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
@@ -114,16 +114,16 @@ function Project() {
         {/* Card 6 */}
         <div className="col-md-4 col-sm-6 mb-4">
           <div className="card">
-            <div className="card-header">Project Title</div>
+            <div className="card-header"><h4>Team Profile Generator</h4></div>
             <img
               src={"http://placehold.it/400x200&text=slide1"}
               alt="boohoo"
               className="img-responsive"
             />
             <div className="card-body">
-              <h5 className="card-text">Description:</h5>
-              <p className="card-text">lorem</p>
-              <a href="#" className="btn btn-primary">
+              <p className="card-text">Description:</p>
+              <p className="card-text">Created using with Node, HTML, CSS, and Bootstrap. Team Profile Generator is a command-line application that accepts user input to generate their team members and information</p>
+              <a href="#" className="btn btn-primary mr-1">
                 Live Production
               </a>
               <a href="#" className="btn btn-primary">
