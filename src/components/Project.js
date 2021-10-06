b5362b3b5d7e5a69fc354e860583a8d5ed91ebba
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-06 16:32:36 -0400

    created layout for project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,137 @@
 import React from "react";
-import "../styles/Project.css";
+// import "../styles/Project.css";
 
 function Project() {
   return (
-    <div class="card">
-      <div class="card-header">Project Title</div>
-      <img src={"http://placehold.it/400x20&text=slide1"} alt="boohoo" className="img-responsive" />
-      <div class="card-body">
-        <h5 class="card-text">Description:</h5>
-        <p class="card-text">lorem</p>
-        <a href="#" class="btn btn-primary">
-          Live Production
-        </a>
-        <a href="#" class="btn btn-primary">
-          Source Code
-        </a>
+    <div className="row d-flex justify-content-around mt-5 px-5">
+      {/* Card 1 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Card 2 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Card 3 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Card 4 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Card 5 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Card 6 */}
+      <div className="col-md-4 col-sm-6 mb-4">
+        <div className="card">
+          <div className="card-header">Project Title</div>
+          <img
+            src={"http://placehold.it/400x200&text=slide1"}
+            alt="boohoo"
+            className="img-responsive"
+          />
+          <div className="card-body">
+            <h5 className="card-text">Description:</h5>
+            <p className="card-text">lorem</p>
+            <a href="#" className="btn btn-primary">
+              Live Production
+            </a>
+            <a href="#" className="btn btn-primary">
+              Source Code
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+export default Project;
