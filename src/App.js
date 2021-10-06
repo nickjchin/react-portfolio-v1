51fe2b96e815b6c2ae88af1097b23b39bec8fa89
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-10-06 12:19:11 -0400

    imported react and components. Setup page layout in App() function

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
